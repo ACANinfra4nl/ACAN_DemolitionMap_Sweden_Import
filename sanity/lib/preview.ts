5dd91faf0cf10b4ce39bddcd4cf4785f3171f0f1
Daniel Hägglund <daniel.hagglund@doberman.co>
2023-09-13 13:50:26 +0200

    simpler preview url

--- sanity/lib/preview.ts
+++ sanity/lib/preview.ts
@@ -1,19 +1,12 @@
 import { StructureBuilder } from "sanity/desk";
 import Iframe from "sanity-plugin-iframe-pane";
 
-export const createPreview = (S: StructureBuilder) => {
-  console.log("createPreview for url", process.env.NEXT_PUBLIC_STUDIO_URL);
-  return [
-    S.view.form(),
-    S.view
-      .component(Iframe)
-      .options({
-        url: `${
-          process.env.NEXT_PUBLIC_STUDIO_URL
-            ? `https://${process.env.NEXT_PUBLIC_STUDIO_URL}`
-            : "http://localhost:3000"
-        }/api/preview`,
-      })
-      .title("Preview"),
-  ];
-};
+export const createPreview = (S: StructureBuilder) => [
+  S.view.form(),
+  S.view
+    .component(Iframe)
+    .options({
+      url: `${document.location.origin}/api/preview`,
+    })
+    .title("Preview"),
+];
