5978b07669cbd0cf1e863f0af8bf7bb62a0bf8cb
Daniel Hägglund <daniel.hagglund@doberman.co>
2023-09-13 11:37:37 +0200

    debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { Navigation } from "@/components/Navigation";
+import { FC } from "react";
+
+const EnvOrNo: FC<{ envVar: string }> = ({ envVar }) => (
+  <p>
+    {envVar}: {process.env[envVar] ?? "Missing"}{" "}
+  </p>
+);
 
 export default function HomePage() {
   return (
-    <main>
+    <>
       <Navigation />
-    </main>
+      <main className="p-4">
+        <EnvOrNo envVar="VERCEL_URL" />
+        <EnvOrNo envVar="NEXT_PUBLIC_VERCEL_URL" />
+      </main>
+    </>
   );
 }
