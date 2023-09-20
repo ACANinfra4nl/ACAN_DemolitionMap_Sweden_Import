50c12bc3fc6972917c54005deee607298bd13363
Daniel Hägglund <daniel.hagglund@doberman.co>
2023-09-20 11:17:33 +0200

    fix state icon

--- src/components/StateIcon.tsx
+++ src/components/StateIcon.tsx
@@ -4,12 +4,12 @@ import { FC } from "react";
 export const StateIcon: FC<{ state: string }> = ({ state }) => (
   <div
     className={classNames(
-      "w-6 h-6 rounded-full",
+      "h-6 w-6 rounded-full",
       state == "riven"
         ? "bg-red-600"
-        : state == "hoted"
+        : state == "hotad"
         ? "bg-yellow-400"
-        : "bg-green-500"
+        : "bg-green-500",
     )}
     aria-label={state}
   ></div>
