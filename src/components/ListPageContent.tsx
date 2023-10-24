6bea282f9beeb4f97c4a5a9b8842e659fa7b0a0d
Daniel Hägglund <daniel.hagglund@doberman.co>
2023-10-24 10:24:17 +0200

    responsive images in list

--- src/components/ListPageContent.tsx
+++ src/components/ListPageContent.tsx
@@ -270,6 +270,7 @@ export const ListPageContent = () => {
                       <BuildingImage
                         images={building.properties.images}
                         state={building.properties.state}
+                        sizes="(min-width: 1600px) 16vw, (min-width: 1024px) 25vw, (min-width: 768px) 33vw, (min-width: 640px) 49vw, 99vw"
                       />
                       <BuildingHeading building={building.properties} list />
                     </a>
