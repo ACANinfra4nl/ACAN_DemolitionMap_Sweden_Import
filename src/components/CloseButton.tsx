191299d4af08127d1c90cef6803261d74f1f6644
Daniel Hägglund <daniel.hagglund@doberman.co>
2023-11-10 17:18:52 +0100

    update carousel style (#3)
    * update carousel style
    
    * hover on carousel and close button

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -19,7 +19,7 @@ export const CloseButton: FC<CloseButtonProps> = ({ onClick }) => {
     <button
       aria-label="Stäng"
       onClick={onClick}
-      className="flex h-12 w-12 items-center justify-center text-5xl font-bold leading-none text-white"
+      className="flex h-12 w-12 items-center justify-center text-5xl font-bold leading-none text-white hover:text-acan-blue"
     >
       &times;
     </button>
