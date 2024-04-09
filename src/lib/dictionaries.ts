2090cf8e4f8d7224bcbc7233349542c2bfa7c2a9
Kicki-Halmos <kickihalmos@gmail.com>
2024-04-09 15:00:13 +0200

    remove workaround

--- src/lib/dictionaries.ts
+++ src/lib/dictionaries.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-const locale = process.env.LANGUAGE || "sv";
+const locale = process.env.LANGUAGE;
 const dictionaries = {
   sv: () => import("../dictionaries/sv.json").then((module) => module.default),
   /*   no: () => import("./dictionaries/no.json").then((module) => module.default),
