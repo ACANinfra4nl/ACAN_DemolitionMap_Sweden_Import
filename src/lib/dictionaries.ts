63a5d5e1e25d3fc68b84ba17d3799b26afca6b8b
Kicki-Halmos <kickihalmos@gmail.com>
2024-04-24 17:17:52 +0200

    implement finnish translation

--- src/lib/dictionaries.ts
+++ src/lib/dictionaries.ts
@@ -3,8 +3,8 @@ import "server-only";
 const locale = process.env.LANGUAGE;
 const dictionaries = {
   sv: () => import("../dictionaries/sv.json").then((module) => module.default),
-  /*   no: () => import("./dictionaries/no.json").then((module) => module.default),
-  fi: () => import("./dictionaries/fi.json").then((module) => module.default), */
+  /*     no: () => import("./dictionaries/no.json").then((module) => module.default), */
+  fi: () => import("../dictionaries/fi.json").then((module) => module.default),
 };
 
 export const getDictionary = async () =>
