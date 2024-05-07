daa0cf6b2489f7bea3257d4b24560a2949e66756
Kicki-Halmos <kickihalmos@gmail.com>
2024-05-07 11:13:01 +0200

    placeholder circlelogo fin

--- src/components/AcanLogoCircle.tsx
+++ src/components/AcanLogoCircle.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 import { FC } from "react";
 import { AcanLogoCircleSwe } from "./AcanLogoCircleSwe";
 import { AcanLogoCircleNo } from "./AcanLogoCircleNo";
+import { AcanLogoCircleFin } from "./AcanLogoCircleFin";
 
 export const AcanLogoCircle: FC = () => {
   if (process.env.LANGUAGE === "fi") {
-    return <AcanLogoCircleSwe />;
+    return <AcanLogoCircleFin />;
   } else if (process.env.LANGUAGE === "sv") {
     return <AcanLogoCircleSwe />;
   } else {
