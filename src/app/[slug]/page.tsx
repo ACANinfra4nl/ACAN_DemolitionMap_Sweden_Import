4c8eacf5b9332064399829af7e32f7bbc9155498
Daniel Hägglund <daniel.hagglund@doberman.co>
2024-08-14 10:59:24 +0200

    remove forced revalidation every hour

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -14,8 +14,6 @@ export function generateStaticParams() {
   return [{ slug: dict.slugs.list }, { slug: dict.slugs.map }];
 }
 
-export const revalidate = 3600;
-
 export default function SlugPage({ params }: { params: { slug: string } }) {
   switch (params.slug) {
     case dict.slugs.list:
